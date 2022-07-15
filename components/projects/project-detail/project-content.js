212676a1521afed9f225a92d372b8125fda06641
beamchristian <ckbeamcoding@gmail.com>
2022-07-15 19:06:13 -0400

    rendering code snippets with syntax Highlighter

--- components/projects/project-detail/project-content.js
+++ components/projects/project-detail/project-content.js
@@ -1,5 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import ProjectHeader from './project-header';
 import classes from './project-content.module.css';
@@ -38,6 +40,17 @@ function ProjectContent(props) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { language, value } = code;
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={value}
+        />
+      );
     },
   };
 
