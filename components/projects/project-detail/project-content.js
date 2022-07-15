364a0a344112f47d9995b2389f5183d70fd9bacb
beamchristian <ckbeamcoding@gmail.com>
2022-07-15 18:43:19 -0400

    project-content images rendering correctly

--- components/projects/project-detail/project-content.js
+++ components/projects/project-detail/project-content.js
@@ -1,4 +1,5 @@
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 
 import ProjectHeader from './project-header';
 import classes from './project-content.module.css';
@@ -8,7 +9,36 @@ function ProjectContent(props) {
   const imagePath = `/images/projects/${project.slug}/${project.image}`;
 
   const customRenderers = {
-    image(image) {},
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/projects/${project.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].type === 'image') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/projects/${project.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
   };
 
   return (
